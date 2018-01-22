a5af4d3293bdaad4a72c0b15ad731353750bb2d1
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2018-01-22 18:31:47 -0500

    Setup environment

---
+++ 01_helloWorld/main.go
@@ -0,0 +1,7 @@
+package main // normally uses name of folder
+
+import "fmt" // can import multiple via ()
+
+func main() {
+	fmt.Println("Hello world!")
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Following Todd McLeod's tutorials:
+# https://www.udemy.com/learn-how-to-code/
+
+# NOTE: Package names are not idiomatic for this repo
