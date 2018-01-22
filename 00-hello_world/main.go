41991a9c30ec9363a479ef31011083559d9bcafc
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2018-01-22 17:17:36 -0500

    initial commit

---
+++ 00-hello_world/main.go
@@ -0,0 +1,13 @@
+
+// package the code belongs to
+package main
+
+// package imported
+// can import multiple via ()
+import "fmt"
+
+// main code that runs automatically
+func main() {
+	fmt.Println("Hello world!")
+	// .Print prints without line break
+}
