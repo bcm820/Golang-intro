afa9c4926af4f68e79c885bf7c67807bbfdcc58e
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2018-01-24 09:46:38 -0500

    scope and closures

--- 03_packages/packages/packages.go
+++ 03_packages/packages/packages.go
@@ -12,3 +12,11 @@ func Exported() string {
 
 // Name is an exported variable
 var Name = "Brian"
+
+/*
+
+Everything within this package is accessible from every .go file in the same package, because they share package scope. In order to access its exported (visible) variables/functions from outside the package, it must be imported.
+
+The ONLY exception to this is the main package. You can't put everything in your main package and access everything across files. One main file with main func. Or, when you run your program, call "go run *.go" and it will run everything.
+
+*/
---
+++ 05_scope/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func wrapper() func() int {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}
+}
+
+func main() {
+	increment := wrapper()
+	fmt.Println(increment())
+	fmt.Println(increment())
+}
+
+/*
+
+2 main types of scope:
+1. package scope
+2. block scope
+
+closure helps us limit the scope of variables used by multiple functions
+without closure, for two or more funcs to have access to the same variable,
+that variable would need to be package scope
+
+in this example, x is scoped within the wrapper func, which when called, returns an anonymous func that increments and returns x (which it has access to since it is inside of wrapper's scope).
+
+*/
